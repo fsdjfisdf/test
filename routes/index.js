80bc2e493c6858333635022e9343bc90625e204c
fsdjfisdf <165735907+fsdjfisdf@users.noreply.github.com>
2024-04-24 16:58:52 +0900

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const fs = require('fs');
+const cors = require('cors');
+const path = require('path');
+const bodyParser = require('body-parser');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index.html');
-});
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.json())
+app.use(cors())
 
-module.exports = router;
+const port = 4000;
+app.listen(port, () => {
+    console.log("Server listening on port", port)
+})
+
+
+app.use('/api/movie',require('./routes/movieRouter'));
